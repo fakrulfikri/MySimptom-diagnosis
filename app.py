b0137e8171886d5bfee201b6d186141322654661
fakrulfikri <82137477+fakrulfikri@users.noreply.github.com>
2021-04-12 17:31:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,6 @@ disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reactio
 
 # TRAINING DATA tr --------------------------------------------------------------------------------
     tr=pd.read_csv("Testing.csv")
-tr=pd.read_csv(os.path.abspath(os.path.dirname(__file__).replace("",""))+"/asset/Testing.csv")
     tr.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
         'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
         'Migraine':11,'Cervical spondylosis':12,
