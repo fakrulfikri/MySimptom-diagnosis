1704e496272c630a27e74223fd0b16c3a011d364
fakrulfikri <82137477+fakrulfikri@users.noreply.github.com>
2021-04-12 21:19:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict(sym1, sym2, sym3, sym4, sym5):
 
 # from gui_stuff import *
 
-    l1=['back pain','constipation','abdominal pain','diarrhoea','mild fever','yellow urine',
+    l1=['sakit belakang','constipation','abdominal pain','diarrhoea','mild fever','yellow urine',
         'acute liver failure','swelling of stomach','swelled lymph nodes','blurred and distorted vision',
         'phlegm','throat irritation','redness of eyes','runny nose','congestion',
         'chest pain','weakness in limbs','fast heart rate','bloody stool','irritation in anus','neck pain',
